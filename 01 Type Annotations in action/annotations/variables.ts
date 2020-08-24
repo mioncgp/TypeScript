048cdba111e70f96f8ca61d55ed7281792e6bc12
Michael <mioncgp@gmail.com>
2020-08-24 21:56:28 +0400

    d

--- 01 Type Annotations in action/annotations/variables.ts
+++ 01 Type Annotations in action/annotations/variables.ts
@@ -6,12 +6,11 @@ let boolean: boolean = true;
 let now: Date = new Date();
 
 // array
-let colors: string[] = ["red", "black"]; // array that contains strings
+let colors: string[] = ["sss", "black", "ss"]; // array that contains strings
 let numbers: number[] = [1, 2, 4];
 let falseTrue: boolean[] = [true, true, false];
 
 // classes
-
 class Car {}
 
 let car: Car = new Car();
@@ -21,3 +20,24 @@ let point: { x: number; y: number } = {
   x: 10,
   y: 20,
 };
+
+// functions
+const logNum: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+// generally, "type inference" is preferred over "type annotations"
+//  except 3 cases:
+// 1) when a function returns the "any" type and we need to clarify the value
+const json = '{"x": 10, "y: 20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+
+// 2) when we declare a variable on one line then initialize it later
+const arr = ["1", "2"];
+let falsy: boolean;
+for (let i = 0; i < 10; i++) {
+  if (arr[i] === "1") {
+    falsy = true;
+  }
+}
+// 3) when we want a variable to have a tpye that can't be inferred correctly
