3913b2713b402d63c8c2fba7c560a19a694787e2
Michael <mioncgp@gmail.com>
2020-08-24 11:22:50 +0400

    d

---
+++ 01 Type Annotations in action/annotations/variables.ts
@@ -0,0 +1,23 @@
+let apples: number = 5; // the colon and the word after the variale is our type annotations
+let speed: string = "lol";
+let boolean: boolean = true;
+
+// build in objects
+let now: Date = new Date();
+
+// array
+let colors: string[] = ["red", "black"]; // array that contains strings
+let numbers: number[] = [1, 2, 4];
+let falseTrue: boolean[] = [true, true, false];
+
+// classes
+
+class Car {}
+
+let car: Car = new Car();
+
+// object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
--- 01 Type Annotations in action/app.js
+++ 01 Type Annotations in action/app.js
@@ -1,7 +1,5 @@
 // types of values:
 // - 1) primitive 7 types: number, boolean, void, undefined, string, symbol, null:
 // - 2) object types: functions, arrays, classes, objects
-
 // Type annotaions = Code we add to tell TS what type of value a variable will refer to
-
 // Type inference = TS tries to figure out what type of value a variable refers to
